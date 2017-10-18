faaf25a2ab6b0a46ed19526b9c7901e6ca7485ef
truetsas <andreas@truetschel.net>
2017-10-18 11:21:51 +0200

    Added domain-services project

---
+++ src/AI4E.Domain.Services/DomainEventAggregator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace AI4E.Domain.Services
+{
+    public class DomainEventAggregator : IDomainEventAggregator
+    {
+        private readonly IEventProcessor<IDomainEvent<Guid>, AggregateRoot> _eventProcessor;
+
+        public DomainEventAggregator(IEventProcessor<IDomainEvent<Guid>, AggregateRoot> eventProcessor)
+        {
+            if (eventProcessor == null)
+                throw new ArgumentNullException(nameof(eventProcessor));
+
+            _eventProcessor = eventProcessor;
+        }
+
+        public void Add<TEvent>(AggregateRoot aggregate, TEvent evt) where TEvent : IDomainEvent<Guid>
+        {
+            if (aggregate == null)
+                throw new ArgumentNullException(nameof(aggregate));
+
+            if (evt == null)
+                throw new ArgumentNullException(nameof(evt));
+
+            _eventProcessor.RegisterEvent(aggregate, evt);
+        }
+    }
+
+    public sealed class DomainEventAggregatorBinder : IEventProcessorBinder<IDomainEvent<Guid>, DomainEventPublisher, AggregateRoot>
+    {
+        public void BindProcessor(DomainEventPublisher eventPublisher, IEventProcessor<IDomainEvent<Guid>, AggregateRoot> eventProcessor)
+        {
+            if (eventPublisher == null)
+                throw new ArgumentNullException(nameof(eventPublisher));
+
+            if (eventProcessor == null)
+                throw new ArgumentNullException(nameof(eventProcessor));
+
+            eventPublisher.BindAggregator(new DomainEventAggregator(eventProcessor));
+        }
+
+        public void UnbindProcessor(DomainEventPublisher eventPublisher)
+        {
+            if (eventPublisher == null)
+                throw new ArgumentNullException(nameof(eventPublisher));
+
+            eventPublisher.ReleaseAggregator();
+        }
+    }
+}
